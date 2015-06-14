614be083b3c991aff57a4c3ea9575140fbf32704
Calder Coalson <caldercoalson@gmail.com>
2015-06-14 13:50:16 -0700

    Cleaned up metagallery.rb.

--- _plugins/metagallery.rb
+++ _plugins/metagallery.rb
@@ -1,14 +1,9 @@
-require 'fastimage'
-
 module Jekyll
 
   class MetaGallery < Liquid::Block
     def render(context)
-      page = File.dirname(context.environments.first["page"]["path"]) + "/"
-      links = @nodelist[0].lines
-                           .map(&:strip)
-                           .reject(&:empty?)
-                           .each_with_index.map{|line, index|
+      links = @nodelist[0].lines.map(&:strip).reject(&:empty?)
+                          .each_with_index.map{|line, index|
         thumbnail, caption = line.split("=").map(&:strip)
         path = thumbnail.split("/")[0..-2].join("/")
 
